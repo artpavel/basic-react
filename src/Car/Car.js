2abbb5d0fd81262a8884a0a208915c2922da3f22
artpavel <artpavel@ukr.net>
2021-03-01 19:51:10 +0200

    create branch

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -2,6 +2,6 @@ export default props =>(
     <div>
         <h2>Car name: {props.name} </h2>
         <p>Year: <strong>{props.year}</strong></p>
-        {props.children}
+        <button onClick={props.onChangeTitle}>Click</button>
     </div>
 )
