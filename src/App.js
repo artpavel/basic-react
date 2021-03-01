b401fb4021371c9ef025122c4f873a29794bf149
artpavel <artpavel@ukr.net>
2021-03-01 15:57:09 +0200

    create branch

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import React from 'react'
 import './App.css';
 import Car from "./Car/Car";
+import {Component} from "react/cjs/react.production.min";
 
-const App = props => {
+class App extends Component {
 
-  const divStyle = {
-    textAlign: 'center',
-    color: 'red',
-    backgroundColor: 'yellow'
-  }
-    /*
-  return (
-      <div style={divStyle}>
-          <h1 style={{padding: '10px', fontSize: '36px'}}>Hello Pavel</h1>
-      </div>
-  );*/
+    state = {
+        cars: [
+            {name: 'Ford', year: 2018},
+            {name: 'Subaru', year: 2021},
+            {name: 'Mitsubishi', year: 2019},
+        ],
+        pageTitle: 'React Components'
+    }
 
-  return (
-      <div style={divStyle}>
-        <h1 style={{padding: '10px', fontSize: '36px'}}>Hello Pavel</h1>
-        <Car name={'Ford'} year={2018} />
-          <Car name={'Subaru'} year={2021}>
-              <p style={{color: 'black', fontSize: '30px'}}>COLOR</p>
-          </Car>
-        <Car name={'Mitsubishi'} year={2019}/>
-      </div>
-  );
+    changeTitleHandler = () => {
+        const oldTitle = this.state.pageTitle
+        const newTitle = oldTitle + ' (changed)'
+        this.setState({
+            pageTitle: newTitle
+        })
+    }
+
+    render() {
+        const divStyle = {
+            textAlign: 'center',
+            color: 'red',
+            backgroundColor: 'yellow'
+        }
+
+        const cars = this.state.cars
+
+        return (
+            <div style={divStyle}>
+                <h1 style={{padding: '10px', fontSize: '36px'}}>{this.state.pageTitle}</h1>
+
+                <button onClick={this.changeTitleHandler}>Change title</button>
+
+                <Car name={cars[0].name} year={cars[0].year}/>
+                <Car name={cars[1].name} year={cars[1].year}/>
+                <Car name={cars[2].name} year={cars[2].year}/>
+            </div>
+        );
+    }
 }
 
 export default App;
